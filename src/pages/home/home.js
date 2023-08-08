d8f17f1a560e5aff7e8c7395ae41874be9986ed1
Naincy <naincyk15042004@gmail.com>
2023-08-08 09:50:04 +0530

    Fetching the data

---
+++ src/pages/home/home.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import "./home.css";
+
+const Home = () => {
+    useEffect(()=>{
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
+  return (
+  <>
+  Home Page
+  </>
+  )
+}
+
+export default Home
