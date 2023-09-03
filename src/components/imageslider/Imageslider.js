2edd2c6b41a9d3f8c22627315dd777dbaf188bbd
Naincy <naincyk15042004@gmail.com>
2023-09-03 18:33:05 +0530

    Fixing error

--- src/components/imageslider/Imageslider.js
+++ src/components/imageslider/Imageslider.js
@@ -1,78 +1,78 @@
-import React,{ useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-//  import Btnslider from "./Btnslider";
-import { BsFillArrowLeftSquareFill } from "react-icons/bs";
-import { BsFillArrowRightSquareFill } from "react-icons/bs";
+// import React,{ useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// //  import Btnslider from "./Btnslider";
+// import { BsFillArrowLeftSquareFill } from "react-icons/bs";
+// import { BsFillArrowRightSquareFill } from "react-icons/bs";
 
 
-const Imageslider = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
+// const Imageslider = () => {
+//   const [popularMovies, setPopularMovies] = useState([]);
   
-  useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=e3dafb33dc59e9998e6349775beab18d&language=en-US"
-    )
-      .then((res) => res.json())
-      .then((data) => setPopularMovies(data.results));
-  }, []);
+//   useEffect(() => {
+//     fetch(
+//       "https://api.themoviedb.org/3/movie/popular?api_key=e3dafb33dc59e9998e6349775beab18d&language=en-US"
+//     )
+//       .then((res) => res.json())
+//       .then((data) => setPopularMovies(data.results));
+//   }, []);
 
-  const [slideIndex,setSlideIndex] = useState(0);
+//   const [slideIndex,setSlideIndex] = useState(0);
 
 
-  const nextSlide=() =>{
-      setSlideIndex(slideIndex===1)
-  }
-  const preSlide=() =>{
-  setSlideIndex(slideIndex-1)
-  }
+//   const nextSlide=() =>{
+//       setSlideIndex(slideIndex===1)
+//   }
+//   const preSlide=() =>{
+//   setSlideIndex(slideIndex-1)
+//   }
 
-  return (
-  <>
-      <BsFillArrowLeftSquareFill size={35} onClick={preSlide}/>
-    <div className="flex gap-x-2 overflow-hidden" >
-        {
-        popularMovies.map((movie ) => (
-          <Link
-            style={{ textDecoration: "none", color: "white" }}
-            to={`/movie/${movie.id}`}
-            key={movie.id}
-          >
+//   return (
+//   <>
+//       <BsFillArrowLeftSquareFill size={35} onClick={preSlide}/>
+//     <div className="flex gap-x-2 overflow-hidden" >
+//         {
+//         popularMovies.map((movie ) => (
+//           <Link
+//             style={{ textDecoration: "none", color: "white" }}
+//             to={`/movie/${movie.id}`}
+//             key={movie.id}
+//           >
             
-            <div className=" border-4 border-green-400">
-            <div className="h-[600px] w-[1255px] border-2 border-yellow-100">
-              <img className="w-full h-full"
-                src={`https://image.tmdb.org/t/p/original${
-                  movie && movie.backdrop_path
-                }`}
-                />
-            </div>
-             <div className="posterImage_overlay">
-              <div className="posterImage_title">
-                {movie ? movie.original_title : ""}
-              </div>
-              <div className="posterImage_runtime">
-                {movie ? movie.release_date : ""}
-                <span className="posterImage_rating">
-                  {movie ? movie.vote_average : ""}
-                  <i className="fas fa-star" />{" "}
-                </span>
-              </div>
-              <div className="posterImage_description">
-                {movie ? movie.overview : ""}
-              </div>
-            </div> 
-          </div>
-           </Link>
-        ))} 
-    </div>
-    <BsFillArrowRightSquareFill size={35} onClick={nextSlide}/>
-     {/* <Btnslider moveSlide={nextSlide} direction={"next"}/>
-    <Btnslider moveSlide={preSlide} direction={"prev"}/>  */}
-        </> 
+//             <div className=" border-4 border-green-400">
+//             <div className="h-[600px] w-[1255px] border-2 border-yellow-100">
+//               <img className="w-full h-full"
+//                 src={`https://image.tmdb.org/t/p/original${
+//                   movie && movie.backdrop_path
+//                 }`}
+//                 />
+//             </div>
+//              <div className="posterImage_overlay">
+//               <div className="posterImage_title">
+//                 {movie ? movie.original_title : ""}
+//               </div>
+//               <div className="posterImage_runtime">
+//                 {movie ? movie.release_date : ""}
+//                 <span className="posterImage_rating">
+//                   {movie ? movie.vote_average : ""}
+//                   <i className="fas fa-star" />{" "}
+//                 </span>
+//               </div>
+//               <div className="posterImage_description">
+//                 {movie ? movie.overview : ""}
+//               </div>
+//             </div> 
+//           </div>
+//            </Link>
+//         ))} 
+//     </div>
+//     <BsFillArrowRightSquareFill size={35} onClick={nextSlide}/>
+//      {/* <Btnslider moveSlide={nextSlide} direction={"next"}/>
+//     <Btnslider moveSlide={preSlide} direction={"prev"}/>  */}
+//         </> 
  
        
-  );
-};
+//   );
+// };
 
-export default Imageslider
+// export default Imageslider
 
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,11 @@
 import React from "react";
-//import { useState,useEffect } from "react";
-//import "./home.css";
+import { useState,useEffect } from "react";
+import "./home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-//import { Carousel } from "react-responsive-carousel";
-//import { Link } from "react-router-dom";
+import { Carousel } from "react-responsive-carousel";
+import { Link } from "react-router-dom";
 import MovieList from "../../components/movieList/movieList";
-import Imageslider from "../../components/imageslider/Imageslider";
+//import Imageslider from "../../components/imageslider/Imageslider";
 
 // const Home = () => {
 //   const [popularMovies, setPopularMovies] = useState([]);
@@ -62,73 +62,73 @@ import Imageslider from "../../components/imageslider/Imageslider";
 // export default Home;
 
 
-const Home = () => {
-  return(
-    <div className="flex flex-col gap-y-">
-      <Imageslider/>
-      <MovieList/>
-    </div>
-  )
-}
-
-export default Home
-
 // const Home = () => {
-//   const [popularMovies, setPopularMovies] = useState([]);
-//   useEffect(() => {
-//     fetch(
-//       "https://api.themoviedb.org/3/movie/popular?api_key=e3dafb33dc59e9998e6349775beab18d&language=en-US"
-//     )
-//       .then((res) => res.json())
-//       .then((data) => setPopularMovies(data.results));
-//   }, []);
+//   return(
+//     <div className="flex flex-col gap-y-">
+//       <Imageslider/>
+//       <MovieList/>
+//     </div>
+//   )
+// }
+
+// export default Home
+
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=e3dafb33dc59e9998e6349775beab18d&language=en-US"
+    )
+      .then((res) => res.json())
+      .then((data) => setPopularMovies(data.results));
+  }, []);
 
 
-//   return (
-//       <div className="flex flex-col gap-y-16">
-//         <Carousel
-//           showThumbs={false}
-//           autoPlay={true}
-//           transitionTime={1}
-//           infiniteLoop={true}
-//           showStatus={false}
-//         >
-//           {popularMovies.map((movie) => (
-//             <Link
-//               style={{ textDecoration: "none", color: "white" }}
-//               to={`/movie/${movie.id}`}
-//               key={movie.id}
+  return (
+      <div className="flex flex-col gap-y-16">
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={1}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+          {popularMovies.map((movie) => (
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to={`/movie/${movie.id}`}
+              key={movie.id}
 
-//             >
-//               <div className="posterImage">
-//                 <img
-//                   src={`https://image.tmdb.org/t/p/original${
-//                     movie && movie.backdrop_path
-//                   }`}
-//                 />
-//               </div>
-//               <div className="posterImage_overlay">
-//                 <div className="posterImage_title">
-//                   {movie ? movie.original_title : ""}
-//                 </div>
-//                 <div className="posterImage_runtime">
-//                   {movie ? movie.release_date : ""}
-//                   <span className="posterImage_rating">
-//                     {movie ? movie.vote_average : ""}
-//                     <i className="fas fa-star" />{" "}
-//                   </span>
-//                 </div>
-//                 <div className="posterImage_description">
-//                   {movie ? movie.overview : ""}
-//                 </div>
-//               </div>
-//             </Link>
-//           ))}
-//         </Carousel>
-//         <MovieList/>
+            >
+              <div className="posterImage">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${
+                    movie && movie.backdrop_path
+                  }`}
+                />
+              </div>
+              <div className="posterImage_overlay">
+                <div className="posterImage_title">
+                  {movie ? movie.original_title : ""}
+                </div>
+                <div className="posterImage_runtime">
+                  {movie ? movie.release_date : ""}
+                  <span className="posterImage_rating">
+                    {movie ? movie.vote_average : ""}
+                    <i className="fas fa-star" />{" "}
+                  </span>
+                </div>
+                <div className="posterImage_description">
+                  {movie ? movie.overview : ""}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </Carousel>
+        <MovieList/>
         
-//       </div>
-//   );
-// };
+      </div>
+  );
+};
 
-// export default Home;
+export default Home;
